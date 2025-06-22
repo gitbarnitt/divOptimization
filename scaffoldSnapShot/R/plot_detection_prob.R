f2e9a6d3ae991a9dcd00f0f3ccc0b3e202302971
gitbarnitt <barnettyard@gmail.com>
2025-06-22 00:43:50 -0600

    stupid ai

--- scaffoldSnapShot/R/plot_detection_prob.R
+++
@@ -1,7 +0,0 @@
-plot_detection_prob <- function(df) {
-  ggplot(df, aes(x = species, y = mean, ymin = lower, ymax = upper)) +
-    geom_pointrange() +
-    facet_wrap(~site, scales = "free") +
-    theme_bw() +
-    labs(title = "Detection Probability Estimates", y = "Probability", x = "Species")
-}
